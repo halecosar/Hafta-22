6b61fcc6d3bb52af48c5f181fce15a7f2090b7aa
halecosar <halealtunakar@hotmail.com>
2024-02-12 21:06:59 +0300

    sections added

--- sport_center_via_react/src/App.jsx
+++ sport_center_via_react/src/App.jsx
@@ -1,5 +1,8 @@
 import './App.css'
 import Section1 from './components/section1'
+import Section2 from './components/section2'
+import Section3 from './components/section3'
+import Section4 from './components/section4'
 
 import React from 'react'
 
@@ -7,6 +10,10 @@ function App() {
   return (
     <div>
       <Section1></Section1>
+      <Section2></Section2>
+      <Section3></Section3>
+      <Section4></Section4>
+
     </div>
   )
 }
---
+++ sport_center_via_react/src/components/section4.jsx
@@ -0,0 +1,124 @@
+import React from 'react'
+
+function Section4() {
+    return (
+        <div>
+            <section id="review">
+                <div className="purchase">
+                    <p style={{ height: '90px' }}></p>
+                    <h2> PURCHASE FROM US</h2>
+                    <div className="orangeLine">
+                    </div>
+
+                    <p className="purchase-p">Lorem ipsum dolor, sit
+                        amet
+                        consectetur adipisicing elit. Deserunt eaque beatae deleniti quibusdam magni temporibus
+                        recusandae dolor
+                        expedita consequatur aperiam?</p>
+
+
+
+
+                    <div className="purchaseImages">
+                        <div className="product-container">
+                            <img src="Resimler/purchase1.jpg" alt="pr.3" className="imgPurchase" />
+                            <p className="product-price">KettleBell / 5</p>
+                            <div className="price-container">
+                                <div className="price-container-text">
+                                    <p style={{ textDecoration: 'line-through' }}>89,99$</p>
+                                    <p>/</p>
+                                    <p>59,99$</p>
+                                </div>
+                                <div className="price-texts">
+                                    <p>Add to Cart</p>
+                                    <button className="add-to-cart">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" width="16" height="16">
+
+                                            <path
+                                                d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
+                                        </svg>
+
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="product-container">
+                            <img src="Resimler/purchase2.jpg" alt="pr.3" className="imgPurchase" />
+                            <p className="product-price">Adjustable Dumbell</p>
+                            <div className="price-container">
+                                <div className="price-container-text">
+                                    <p style={{ textDecoration: 'line-through' }}>89,99$</p>
+                                    <p>/</p>
+                                    <p>59,99$</p>
+                                </div>
+                                <div className="price-texts">
+                                    <p>Add to Cart</p>
+                                    <button className="add-to-cart">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" width="16" height="16">
+
+                                            <path
+                                                d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
+                                        </svg>
+
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="product-container">
+                            <img src="Resimler/purchase3.jpg" alt="pr.3" className="imgPurchase" />
+                            <p className="product-price">Adjustable Dumbell</p>
+                            <div className="price-container">
+                                <div className="price-container-text">
+                                    <p style={{ textDecoration: 'line-through' }}>89,99$</p>
+                                    <p>/</p>
+                                    <p>59,99$</p>
+                                </div>
+                                <div className="price-texts">
+                                    <p>Add to Cart</p>
+                                    <button className="add-to-cart">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" width="16" height="16">
+
+                                            <path
+                                                d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
+                                        </svg>
+
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="product-container">
+
+                            <img src="Resimler/purchase4.jpg" alt="pr.3" className="imgPurchase" />
+                            <p className="product-price">Adjustable Dumbell</p>
+                            <div className="price-container">
+                                <div className="price-container-text">
+                                    <p style={{ textDecoration: 'line-through' }}>89,99$</p>
+                                    <p>/</p>
+                                    <p>59,99$</p>
+                                </div>
+                                <div className="price-texts">
+                                    <p>Add to Cart</p>
+                                    <button className="add-to-cart">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" width="16" height="16">
+
+                                            <path
+                                                d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
+                                        </svg>
+
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </section>
+
+        </div>
+
+
+    )
+}
+
+export default Section4;
